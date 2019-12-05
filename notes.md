cb2c2ec68dee22ade9e9bff928d995abe732976b
cjogles <jackson.ogles28@gmail.com>
2019-12-05 15:12:26 -0700

    file structure and login form added

---
+++ notes.md
@@ -0,0 +1,12 @@
+# Notes
+
+- login (authenticate user with a password)
+- create user / add user to database
+- edit user
+- delete user
+- get users posts
+- edit users posts
+- add posts to a user
+- delete posts from a user
+
+
---
+++ src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import react from 'react';
+
+const loginForm = () => {
+    const 
+    return (
+        <div>
+            <form onSubmit={}>
+                <label> 
+                    Username
+                    <input 
+                        type="text"
+                        name="username"
+                        value={}
+                        onChange={}
+                        />
+                </label>
+                <label> 
+                    Password
+                    <input 
+                        type="password"
+                        name="password"
+                        value={}
+                        onChange={}
+                        />
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default loginForm;
